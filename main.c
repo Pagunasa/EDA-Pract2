2b880da85c589e766e004324afa0baf1f97e2e4a
u140172 <u140172@54006p37.aulesca.upf.edu>
2017-02-21 11:00:56 +0100

    First Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: u140172
+ *
+ * Created on February 21, 2017, 10:56 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
